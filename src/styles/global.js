de17d0ed7200465405cbac4154099482a32e1ef1
pierrick <p.leroy.mtl@gmail.com>
2020-07-23 11:21:58 -0400

    removed import fonts in global

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Parisienne&display=swap');
     :root {
         box-sizing: border-box;
     }
